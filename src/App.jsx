b6e6a9eff2bda6e61f3988af6cdb3b83dcef13aa
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-05-04 12:26:31 +0530

    adds book-collection table in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+class Table extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  render() {
+    return (
+      <tr>
+        <td>{this.props.title}</td>
+        <td>{this.props.author}</td>
+        <td>{this.props.year}</td>
+      </tr>
+    );
+  }
 }
 
-export default App
+class BookCollection extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello</h1>
+        <div className="table-container">
+          <h1>Book Collection</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Table
+                title={"Great Expectation"}
+                author={"Charles Dickens"}
+                year={1860}
+              />
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
+}
+
+export default BookCollection;
