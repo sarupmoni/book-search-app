fb71ca0d198f915c87f00681e71db04e81b9b95f
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-05-04 13:48:55 +0530

    adds search-book

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,37 @@ class Table extends Component {
 }
 
 class BookCollection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        { title: "Great Expectation", author: "Charles Dickens", year: 1860 },
+        { title: "War and Peace", author: "Leo Tolstoy", year: 1869 },
+        {
+          title: "The Great Gatsby",
+          author: "F. Scott Fitzgerald	",
+          year: 1925,
+        },
+        { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+        { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+      ],
+      searchBooks: [
+        { title: "Great Expectation", author: "Charles Dickens", year: 1860 },
+        { title: "War and Peace", author: "Leo Tolstoy", year: 1869 },
+        {
+          title: "The Great Gatsby",
+          author: "F. Scott Fitzgerald	",
+          year: 1925,
+        },
+        { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+        { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+      ],
+    };
+  }
+
   render() {
     return (
       <>
-        <h1>Hello</h1>
         <div className="table-container">
           <h1>Book Collection</h1>
           <table>
@@ -33,11 +60,14 @@ class BookCollection extends Component {
               </tr>
             </thead>
             <tbody>
-              <Table
-                title={"Great Expectation"}
-                author={"Charles Dickens"}
-                year={1860}
-              />
+              {this.state.searchBooks.map((book, index) => (
+                <Table
+                  key={index}
+                  title={book.title}
+                  author={book.author}
+                  year={book.year}
+                />
+              ))}
             </tbody>
           </table>
         </div>
